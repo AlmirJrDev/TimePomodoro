0cb1cc914c28ef70a9d43dcb1dc3a44f02a637f5
Almir Jr <99154364+AlmirJrDev@users.noreply.github.com>
2022-12-22 09:47:01 -0300

    erro space-between

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <nav>
+        <span>Logo</span>
+        <a href="">Timer</a>
+        <a href="">History</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,14 @@
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
 import { Outlet } from 'react-router-dom'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
     <div>
+      <LayoutContainer>
       <Header />
       <Outlet />
+      </LayoutContainer>
     </div>
   )
 }
