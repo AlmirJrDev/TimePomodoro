d0ce7924335e638120702756b95581025e402ac9
AlmirJrDev <almagomes1@hotmail.com>
2022-12-29 09:38:36 -0300

    Header and Layoults

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer } from './styles'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logoIgnite} alt="" />
       <nav>
-        <a href="">Timer</a>
-        <a href="">History</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
